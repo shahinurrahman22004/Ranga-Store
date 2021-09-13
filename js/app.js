a8be628d67b234a1bf2de35052f4a8c75cb914f8
shahinurrahman22004 <shahinur02345@gmail.com>
2021-09-13 09:55:59 +0600

    second

--- js/app.js
+++ js/app.js
@@ -5,12 +5,11 @@ const loadProducts = () => {
     .then((data) => showProducts(data));
 };
 loadProducts();
-
 // show all product in UI 
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
+    const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
